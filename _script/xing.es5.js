f232a942f3f362346433023f7f9bc7ec3850a0f0
indus <stefan.keim@posteo.de>
2015-12-29 16:57:15 +0100

    layout

---
+++ _script/xing.es5.js
@@ -0,0 +1,177 @@
+﻿/*!
+ * XING
+ *
+ * @author Stefan Keim (indus)
+ * @version 0.1.1
+ * @description canvas based line background
+ *
+ */
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+// MIT license
+'use strict';
+
+"use static";
+var XING = (function () {
+    function XING(el, options) {
+        if (options === void 0) {
+            options = {};
+        }
+        this.options = options;
+        var canvas = typeof el == 'string' || el instanceof String ? document.getElementById(el) : el;
+        if (canvas.tagName != 'CANVAS') throw "no canvas";
+        for (var key in XING.options) {
+            options[key] = options[key] === undefined ? XING.options[key] : options[key];
+        }
+        var ctx = canvas.getContext('2d', { alpha: !options.bkgdColor }),
+            tilt = { x: 0, y: 0 },
+            _,
+            l,
+            w,
+            h,
+            random = Math.random,
+            p = 0;
+        function sort(a, b) {
+            return a[1] - b[1];
+        }
+        ;
+        var ray,
+            x1,
+            y1,
+            x2,
+            y2,
+            s = [],
+            sj,
+            d,
+            fac,
+            ray_,
+            x,
+            y,
+            r,
+            i,
+            j;
+        var update = function update(p_) {
+            options.bkgdColor ? ctx.fillRect(0, 0, w, h) : ctx.clearRect(0, 0, w, h);
+            p = p_ - p || 16;
+            for (i = l; i; i--) {
+                ray = _[i - 1];
+                x1 = ray.x1;
+                y1 = ray.y1;
+                x2 = ray.x2;
+                y2 = ray.y2;
+                if (options.parallax) {
+                    fac = options.parallax / i;
+                    x1 += tilt.x * fac;
+                    x2 += tilt.x * fac;
+                    y1 += tilt.y * fac;
+                    y2 += tilt.y * fac;
+                }
+                ray.m = (y1 - y2) / (x1 - x2);
+                ray.b = y1 - ray.m * x1;
+                x1 = ray.x1 += ray.vx1 * p;
+                y1 = ray.y1 += ray.vx1 * p;
+                x2 = ray.x2 += ray.vx2 * p;
+                y2 = ray.y2 += ray.vy2 * p;
+                (x1 < 0 || x1 > 1) && (ray.vx1 = -ray.vx1);
+                (y1 < 0 || y1 > 1) && (ray.vy1 = -ray.vy1);
+                (x2 < 0 || x2 > 1) && (ray.vx2 = -ray.vx2);
+                (y2 < 0 || y2 > 1) && (ray.vy2 = -ray.vy2);
+                x1 = Math.min(Math.max((+(ray.m < 0) - ray.b) / ray.m, 0), 1), y1 = ray.m * x1 + ray.b;
+                x2 = Math.min(Math.max((+(ray.m > 0) - ray.b) / ray.m, 0), 1), y2 = ray.m * x2 + ray.b;
+                s[s.length = 0] = [0, x1, y1], d = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+                for (j = i; j < l; j++) {
+                    ray_ = _[j], x = (ray_.b - ray.b) / (ray.m - ray_.m), y = ray.m * x + ray.b;
+                    if (x > 0 && x < 1 && y > 0 && y < 1) {
+                        r = .01 / Math.abs(Math.atan(ray.m) - Math.atan(ray_.m)) + 0.01;
+                        s[s.push([1, x - r * (x2 - x1), y - r * (y2 - y1)])] = [0, x + r * (x2 - x1), y + r * (y2 - y1)];
+                    }
+                }
+                s.push([1, x2, y2]);
+                s.sort(sort);
+                ctx.beginPath();
+                for (j = 0; j < s.length && (sj = s[j]); j++) {
+                    if (!j || !sj[0] || s[j - 1][0] != sj[0]) {
+                        sj[0] ? ctx.lineTo(sj[1] * w, sj[2] * h) : ctx.moveTo(sj[1] * w, sj[2] * h);
+                    }
+                }
+                ctx.lineWidth = i * .2 + .4;
+                ctx.stroke();
+            }
+            p = p_;
+            requestAnimationFrame(update);
+        };
+        _ = [];
+        l = 1;
+        while (l++ < Math.max(options.lineNum, 2)) {
+            var v = options.velocity * l / 1e5;
+            l = _.push({
+                x1: random(),
+                y1: random(),
+                x2: random(),
+                y2: random(),
+                vx1: random() * v,
+                vy1: random() * v,
+                vx2: random() * v,
+                vy2: random() * v
+            });
+        }
+        l--;
+        var mouse;
+        function onMousemove(ev) {
+            mouse = true;
+            tilt.x = ev.pageX / (window.innerWidth / 2) - 1;
+            tilt.y = ev.pageY / (window.innerHeight / 2) - 1;
+        }
+        function onOrientation(ev) {
+            if (mouse) return window.removeEventListener('deviceorientation', onOrientation, false);
+            tilt.x = Math.min(Math.max(-ev.gamma / 30, -1), 1);
+            tilt.y = Math.min(Math.max(-ev.beta / 30, -1), 1);
+        }
+        function onResize() {
+            w = canvas.width = (window.innerWidth || canvas.offsetWidth) / 2;
+            h = canvas.height = (window.innerHeight || canvas.offsetHeight) / 2;
+            ctx.fillStyle = options.bkgdColor;
+            ctx.strokeStyle = options.lineColor;
+            ctx.lineCap = "round";
+        }
+        window.addEventListener('resize', onResize, false);
+        document.addEventListener('mousemove', onMousemove, false);
+        //window.addEventListener('deviceorientation', onOrientation, false);
+        onResize();
+        update(0);
+    }
+    XING.options = {
+        velocity: 1,
+        parallax: .1,
+        lineNum: 8,
+        bkgdColor: '#55acee',
+        lineColor: 'rgba(255,255,255,.15)'
+    };
+    return XING;
+})();
+window.addEventListener("load", function load(event) {
+    window.removeEventListener("load", load, false);
+    new XING('xing');
+}, false);
+(function () {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
+    }
+    if (!window.requestAnimationFrame) window.requestAnimationFrame = function (callback, element) {
+        var currTime = new Date().getTime();
+        var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+        var id = window.setTimeout(function () {
+            callback(currTime + timeToCall);
+        }, timeToCall);
+        lastTime = currTime + timeToCall;
+        return id;
+    };
+    if (!window.cancelAnimationFrame) window.cancelAnimationFrame = function (id) {
+        clearTimeout(id);
+    };
+})();
+
---
+++ js/xing.es5.js
@@ -0,0 +1,43 @@
+﻿/*!
+* XING
+*
+* @author Stefan Keim (indus)
+* @version 0.1.1
+* @description canvas based line background
+*
+*/
+"use strict";
+
+(function () {
+  for (var i = 0, t = ["ms", "moz", "webkit", "o"], n = 0; n < t.length && !window.requestAnimationFrame; ++n) window.requestAnimationFrame = window[t[n] + "RequestAnimationFrame"], window.cancelAnimationFrame = window[t[n] + "CancelAnimationFrame"] || window[t[n] + "CancelRequestAnimationFrame"];window.requestAnimationFrame || (window.requestAnimationFrame = function (n) {
+    var t = new Date().getTime(),
+        r = Math.max(0, 16 - (t - i)),
+        u = window.setTimeout(function () {
+      n(t + r);
+    }, r);return i = t + r, u;
+  });window.cancelAnimationFrame || (window.cancelAnimationFrame = function (n) {
+    clearTimeout(n);
+  });
+})();"use static";var XING = (function () {
+  function n(t, i) {
+    function ct(n, t) {
+      return n[1] - t[1];
+    }function at(n) {
+      st = !0;l.x = n.pageX / (window.innerWidth / 2) - 1;l.y = n.pageY / (window.innerHeight / 2) - 1;
+    }function ht() {
+      g = y.width = (window.innerHeight || y.offsetHeight) / 2;nt = y.height = (window.innerWidth || y.offsetWidth) / 2;s.fillStyle = i.bkgdColor;s.strokeStyle = i.lineColor;s.lineCap = "round";
+    }var y, d, r, u, f, e, o, h, v, lt, tt, et, k, it, rt, b, c, ot, ut, st;if ((i === void 0 && (i = {}), this.options = i, y = typeof t == "string" || t instanceof String ? document.getElementById(t) : t, y.tagName != "CANVAS")) throw "no canvas";for (d in n.options) i[d] = i[d] === undefined ? n.options[d] : i[d];var s = y.getContext("2d", { alpha: !i.bkgdColor }),
+        l = { x: 0, y: 0 },
+        ft,
+        p,
+        g,
+        nt,
+        a = Math.random,
+        w = 0;for (h = [], ot = function ot(n) {
+      for (i.bkgdColor ? s.fillRect(0, 0, g, nt) : s.clearRect(0, 0, g, nt), w = n - w || 16, b = p; b; b--) {
+        for (r = ft[b - 1], u = r.x1, f = r.y1, e = r.x2, o = r.y2, i.parallax && (tt = i.parallax / b, u += l.x * tt, e += l.x * tt, f += l.y * tt, o += l.y * tt), r.m = (f - o) / (u - e), r.b = f - r.m * u, u = r.x1 += r.vx1 * w, f = r.y1 += r.vx1 * w, e = r.x2 += r.vx2 * w, o = r.y2 += r.vy2 * w, (u < 0 || u > 1) && (r.vx1 = -r.vx1), (f < 0 || f > 1) && (r.vy1 = -r.vy1), (e < 0 || e > 1) && (r.vx2 = -r.vx2), (o < 0 || o > 1) && (r.vy2 = -r.vy2), u = Math.min(Math.max((+(r.m < 0) - r.b) / r.m, 0), 1), f = r.m * u + r.b, e = Math.min(Math.max((+(r.m > 0) - r.b) / r.m, 0), 1), o = r.m * e + r.b, h[h.length = 0] = [0, u, f], lt = Math.sqrt((e - u) * (e - u) + (o - f) * (o - f)), c = b; c < p; c++) et = ft[c], k = (et.b - r.b) / (r.m - et.m), it = r.m * k + r.b, k > 0 && k < 1 && it > 0 && it < 1 && (rt = .01 / Math.abs(Math.atan(r.m) - Math.atan(et.m)) + .01, h[h.push([1, k - rt * (e - u), it - rt * (o - f)])] = [0, k + rt * (e - u), it + rt * (o - f)]);for (h.push([1, e, o]), h.sort(ct), s.beginPath(), c = 0; c < h.length && (v = h[c]); c++) c && v[0] && h[c - 1][0] == v[0] || (v[0] ? s.lineTo(v[1] * g, v[2] * nt) : s.moveTo(v[1] * g, v[2] * nt));s.lineWidth = b * .2 + .4;s.stroke();
+      }w = n;requestAnimationFrame(ot);
+    }, ft = [], p = 1; p++ < Math.max(i.lineNum, 2);) ut = i.velocity * p / 1e5, p = ft.push({ x1: a(), y1: a(), x2: a(), y2: a(), vx1: a() * ut, vy1: a() * ut, vx2: a() * ut, vy2: a() * ut });p--;window.addEventListener("resize", ht, !1);document.addEventListener("mousemove", at, !1);ht();ot(0);
+  }return n.options = { velocity: 1, parallax: .1, lineNum: 8, bkgdColor: "#55acee", lineColor: "rgba(255,255,255,.15)" }, n;
+})();new XING("xing");
+
